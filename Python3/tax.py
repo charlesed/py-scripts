dbe09ca96fa177564e0fb91dede359f70278d672
Charles Doten <charles@charlesed.net>
2011-04-24 11:27:22 -0500

    Add Files

---
+++ Python3/tax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.1
+
+# Calculating some sales tax...
+
+# What's your tax rate? 
+tax = float(input("Tax rate(%): "))
+tax = tax / 100
+
+# Now what's your price?
+price = float(input("Price: "))
+
+# Do some math and print the total
+tax = price * tax
+total = price + round(tax, 2)
+print(total)
+
+# Wait for user to press enter
+input("Press ENTER to exit")
