02d3a930a3d6ca4eb90c16b2b47211faa4f867b2
Charles Doten <git@cd-mn.us>
2014-04-12 13:46:32 -0500

    Cleaning up

--- Python2/update_dyndns_iptables.py
+++ Python2/update_dyndns_iptables.py
@@ -3,8 +3,8 @@
 import os
 import socket
 
-home_dyndns = ""
-log_dyndns = "/tmp/dyndns.log"
+dyndns_host = ""
+dyndns_log = "/tmp/dyndns.log"
 local_ip = ""
 tcp_ports = ('22')
 udp_ports = ()
@@ -14,31 +14,31 @@ def updatelog(file, new_ip):
     print "Log updated with new IP", new_ip
     file.close()
 
-def updateiptables(old_dynip, new_dynip):
+def updateiptables(old_ip, new_ip):
     print "Updating iptables..."
     for port in tcp_ports:
-        os.system("iptables -D INPUT -s " + old_dynip + " -d " + local_ip + " -p tcp --dport " + port + " -j ACCEPT")
-        os.system("iptables -I INPUT 2 -s " + new_dynip + " -d " + local_ip + " -p tcp --dport " + port + " -j ACCEPT")
+        os.system("iptables -D INPUT -s " + old_ip + " -d " + local_ip + " -p tcp --dport " + port + " -j ACCEPT")
+        os.system("iptables -I INPUT 2 -s " + new_ip + " -d " + local_ip + " -p tcp --dport " + port + " -j ACCEPT")
     for udp_port in udp_ports:
-        os.system("iptables -D INPUT -s " + old_dynip + " -d " + local_ip + " -p udp --dport " + port + " -j ACCEPT")
-        os.system("iptables -I INPUT 2 -s " + new_dynip + " -d " + local_ip + " -p udp --dport " + port + " -j ACCEPT")
+        os.system("iptables -D INPUT -s " + old_ip + " -d " + local_ip + " -p udp --dport " + port + " -j ACCEPT")
+        os.system("iptables -I INPUT 2 -s " + new_ip + " -d " + local_ip + " -p udp --dport " + port + " -j ACCEPT")
     print "iptables updated."
 
 try:
-    log = open(log_dyndns, 'r+')
+    log = open(dyndns_log, 'r+')
 except IOError:
-    log = open(log_dyndns, 'w+')
+    log = open(dyndns_log, 'w+')
 
-last_dyndns = log.read()
-cur_dyndns = socket.gethostbyname(home_dyndns)
+dynip_pre = log.read()
+dynip_cur = socket.gethostbyname(dyndns_host)
 
-print "Log:", last_dyndns
-print "Cur:", cur_dyndns
+print "Log:", dynip_pre
+print "Cur:", dynip_cur
 
-if cur_dyndns != last_dyndns:
-    updatelog(log, cur_dyndns)
-    print "Log updated to new IP.", cur_dyndns
+if dynip_cur != dynip_pre:
+    updatelog(log, dynip_cur)
+    print "Log updated to new IP.", dynip_cur
 else:
     print "Adresses match, not updating log."
 
-updateiptables(last_dyndns, cur_dyndns)
+updateiptables(dynip_pre, dynip_cur)
